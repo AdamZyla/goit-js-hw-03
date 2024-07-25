31031c94a9a5104a2d6ec58dbd23578d56122918
AdamZyla <azyla@interia.pl>
2024-07-25 22:15:52 +0200

    TASK-2

--- task-2.js
+++ task-2.js
@@ -1,2 +1,22 @@
 /*Zadanie 2. Kompozycja tablic*/
 
+function makeArray(firstArray, secondArray, maxLength) {
+    const newArray = [...firstArray, ...secondArray];
+  
+    if (newArray.length >= maxLength) {
+      return newArray.slice(0, maxLength);
+    }
+    
+    return newArray;
+  }
+  
+  console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 5)); 
+  console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+  console.log(makeArray(["Mango"], ["Ajax", "Chelsea"], 2)); 
+  console.log(makeArray(["A"], ["B", "C", "D", "E","F","G"], 6)); 
+
+
+//          (4) ['Mango', 'Poly', 'Ajax', 'Chelsea']
+// VM317:14 (3) ['Mango', 'Poly', 'Ajax']
+// VM317:15 (2) ['Mango', 'Ajax']
+// VM317:16 (6) ['A', 'B', 'C', 'D', 'E', 'F']
