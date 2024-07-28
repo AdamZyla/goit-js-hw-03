8c530ad2f95f07d0de13650e648b1c2a41b9aef9
AdamZyla <azyla@interia.pl>
2024-07-28 16:44:21 +0200

    task-1

--- task-1.js
+++ task-1.js
@@ -1,16 +1,16 @@
 /*Zadanie 1. Generator slug*/
 
-function slugify(title) {
-    return title
-      .toLowerCase()
-      .trim() 
-      .replace(/\s+/g, '-'); 
-  }
-  
-  console.log(slugify("Arrays for begginers")); 
-  console.log(slugify("English for developer"));
-  console.log(slugify("Ten secrets of JavaScript"));
-  console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+  function slugify(title) {
+      return title
+        .toLowerCase()
+        .trim() 
+        .replace(/\s+/g, '-'); 
+    }
+    
+    console.log(slugify("Arrays for begginers")); 
+    console.log(slugify("English for developer"));
+    console.log(slugify("Ten secrets of JavaScript"));
+    console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
 
 //          arrays-for-begginers
 // VM182:12 english-for-developer
